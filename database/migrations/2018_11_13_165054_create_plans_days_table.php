b5946eb9852d372bffddb6185b38914d3c84bdc0
Shobi P P <ppshobi@gmail.com>
2018-11-13 22:23:43 +0530

    plan days table migration

---
+++ database/migrations/2018_11_13_165054_create_plans_days_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlansDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans_days', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('plan_id');
+            $table->integer('order');
+            $table->timestamps();
+
+            $table->foreign('plan_id')->references('id')->on('plans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans_days');
+    }
+}
