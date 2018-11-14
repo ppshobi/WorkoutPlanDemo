46076f0bc5b00dec96dd4abf2e25c911cc9b72c1
Shobi P P <ppshobi@gmail.com>
2018-11-15 01:05:28 +0530

    cleanup

--- app/Http/Controllers/DaysController.php
+++ app/Http/Controllers/DaysController.php
@@ -18,16 +18,6 @@ class DaysController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -40,28 +30,6 @@ class DaysController extends Controller
             'name' => 'Day ' . (Day::where('plan_id', $request->plan_id)->count() + 1),
             'order' => 1,
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
