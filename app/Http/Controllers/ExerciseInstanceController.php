809015270d6cff4b0aa87d2b9665489c43545652
Shobi P P <ppshobi@gmail.com>
2018-11-16 19:16:55 +0530

    cleanup

--- app/Http/Controllers/ExerciseInstanceController.php
+++ app/Http/Controllers/ExerciseInstanceController.php
@@ -8,16 +8,6 @@ use Illuminate\Http\Request;
 class ExerciseInstanceController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -36,18 +26,6 @@ class ExerciseInstanceController extends Controller
         }
 
         return ExerciseInstance::where('day_id', $request->dayId)->get();
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-
     }
 
     public function delete($dayId, $exerciseId)
