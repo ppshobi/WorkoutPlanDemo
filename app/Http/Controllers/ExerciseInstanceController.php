4b69cf41d12d94e0f5c466433e6d4a90e92447fb
Shobi P P <ppshobi@gmail.com>
2018-11-16 19:55:21 +0530

    fix exercise instance observer

--- app/Http/Controllers/ExerciseInstanceController.php
+++ app/Http/Controllers/ExerciseInstanceController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\ExerciseInstance;
+use App\Mail\PlanUpdated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ExerciseInstanceController extends Controller
 {
@@ -25,11 +27,29 @@ class ExerciseInstanceController extends Controller
             ]);
         }
 
+        $this->sendNotificationMail($request->exercises[0]);
+
         return ExerciseInstance::where('day_id', $request->dayId)->get();
     }
 
     public function delete($dayId, $exerciseId)
     {
         return ExerciseInstance::where('day_id', $dayId)->where('exercise_id', $exerciseId)->delete();
+    }
+
+    private function sendNotificationMail($exercise)
+    {
+        if($exercise) {
+            $exerciseInstance = ExerciseInstance::where('day_id', $exercise['day_id'])
+                                                ->where('exercise_id', $exercise['id'])
+                                                ->first();
+            if($exerciseInstance) {
+                foreach ($exerciseInstance->day->plan->users as $user) {
+                    Mail::to($user)
+                        ->send(new PlanUpdated($user));
+                }
+            }
+        }
+
     }
 }
